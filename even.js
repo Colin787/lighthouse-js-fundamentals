523ece9551b5a46b18894a73c265400c1321c311
Colin Park <colinpark4@gmail.com>
2017-05-03 01:38:34 +0000

    Added the function exercise 1 and 2

---
+++ even.js
@@ -0,0 +1,9 @@
+function isEven(num) {
+	return num % 2 ===0;
+}
+
+var ten = isEven(10);
+var elevenOdd = isEven(11);
+
+console.log(ten);
+console.log(elevenOdd);
---
+++ hello.js
@@ -0,0 +1,7 @@
+function sayHello(name) {
+	console.log("Hello, " + name);
+}
+sayHello("Colin");
+sayHello("ManlyMan");
+sayHello("Coolguy");
+

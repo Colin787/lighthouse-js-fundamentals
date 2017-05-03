f86197e35655020f3a666c696c75a3a04f705711
Colin Park <colinpark4@gmail.com>
2017-05-03 02:03:10 +0000

    Just added the isOdd file to Github

---
+++ odd.js
@@ -0,0 +1,7 @@
+function isOdd(num) {
+	return num % 2 === 1;
+}
+var fiveOdd = 5;
+var sixEven = 6;
+console.log("This one evaluates to " + isOdd(fiveOdd));
+console.log("And this one is " + isOdd(sixEven));
